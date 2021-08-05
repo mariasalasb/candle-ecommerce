ebfc608105340361fa510bbc130370c5c0f6aafb
marimarsb <mariamargaritasalasb@gmail.com>
2021-08-04 22:17:32 -0300

    context añadido para item count en cart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,55 @@ import ItemDetail from './components/ItemDetailContainer/ItemDetail';
 import image1 from './assets/lights.jpg'
 import {Container} from "reactstrap";
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import CartContext from './context/CartContext';
+import CartContext, {CartProvider} from './context/CartContext';
+import { Item } from './components/ItemListContainer/Item';
+
+
+function App() {
+
+  function onkeydown(e){
+    e.key==='a' ? e.preventDefault() : console.log(e)
+  }
+
+  /*const[cart,setCart]=useState ([{item:{},quantity:''}])
+
+  const[itemCompra, setItemCompra]=useState({})
+  const [cantidadCompra,setCantidadCompra]=useState()
+  console.log({itemCompra})*/
+
+
+  return (
+    <div className="App">
+      <Router>
+           <NavBar /> {/*va fuera del switch porque permanece en / pero dentro del router porque necesitamos que tenga funcionalidad de navegacion*/}
+        <Switch>
+          {/*<input onKeyDown={onkeydown}></input>*/}
+          <Container fluid>
+              <Route exact path="/"> {/*exact path="/category/:categoryId" lo que va luego de : es parametro*/}
+                <ItemListContainer source={image1} />
+              </Route>
+              <CartProvider>
+                <Route exact path= '/detail/:detailId' component={ItemDetailContainer}/>
+              </CartProvider>
+          </Container>
+        </Switch>
+      </Router>
+    </div>
+  );
+} 
+
+export default  App;
+
+/* import React, {useState, useEffect} from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetail from './components/ItemDetailContainer/ItemDetail';
+import image1 from './assets/lights.jpg'
+import {Container} from "reactstrap";
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import CartContext, {CartProvider} from './context/CartContext';
 import { Item } from './components/ItemListContainer/Item';
 
 
@@ -21,19 +69,19 @@ function App() {
 
   const[itemCompra, setItemCompra]=useState({})
   const [cantidadCompra,setCantidadCompra]=useState()
-  console.log(cantidadCompra)
+  console.log({itemCompra})
 
   return (
     <div className="App">
       <Router>
-           <NavBar /> {/*va fuera del switch porque permanece en / pero dentro del router porque necesitamos que tenga funcionalidad de navegacion*/}
+           <NavBar /> {/*va fuera del switch porque permanece en / pero dentro del router porque necesitamos que tenga funcionalidad de navegacion
         <Switch>
-          {/*<input onKeyDown={onkeydown}></input>*/}
+          {/*<input onKeyDown={onkeydown}></input>
           <Container fluid>
-              <Route exact path="/"> {/*exact path="/category/:categoryId" lo que va luego de : es parametro*/}
+              <Route exact path="/"> {/*exact path="/category/:categoryId" lo que va luego de : es parametro
                 <ItemListContainer source={image1} />
               </Route>
-              <CartContext.Provider value={[cantidadCompra,setCantidadCompra,{itemCompra}, setItemCompra,cart,setCart]}>
+              <CartContext.Provider value={addToCart}>
                 <Route exact path= '/detail/:detailId' component={ItemDetailContainer}/>
               </CartContext.Provider>
           </Container>
@@ -43,5 +91,5 @@ function App() {
   );
 } 
 
-export default  App;
+export default  App; */
 
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,6 +1,43 @@
-import {createContext} from 'react'
+import { createContext, useState } from 'react'
+
+export const CartContext = createContext()
+
+export const CartProvider = ({children}) => {
+    const [cart, setCart] = useState([])
+    console.log(cart)
+
+    const addToCart = (item, quantity) => {
+        if(isInCart(item.id)){
+            quantity += quantity
+            let index = cart.findIndex((e) => e.item.id === item.id)
+            cart.splice(index, 1)
+            setCart([...cart, {'item': item , 'quantity': quantity }])
+            //console.log(getTotal)
+        }else{
+            setCart([...cart, {'item': item , 'quantity': quantity }])
+        }
+ }
+    const isInCart = (id) => {
+        return cart.find((item)=>{
+            if(item.item.id !== id){
+                return false
+            }
+            else{
+                return item.item.id
+            }
+        })
+    }
+    return(
+        <CartContext.Provider value={{addToCart}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
+
+/* import {createContext} from 'react'
 
 const CartContext= createContext()
 
-
-export default CartContext
+export default CartContext */
